369856cdbcbd749c6e17aac71e10202cfe6efc16
LeoDesired <72989672+LeoDesired@users.noreply.github.com>
2020-10-16 12:14:46 -0400

    Create main.py
    Error issue.

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+import pygame
+import random 
+import time
+print('We are starting your game. Please wait one moment.')  
+time.sleep(5) 
+print('Our local server has defined your server. We are now turning it on.') 
+time.sleep(3) 
+print('Our connection has a error. Please try again later.') 
